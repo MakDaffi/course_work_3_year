3ce5f40ac56e1a5d34521d1ec1a56ba9e644f421
MakDaffi <63809925+MakDaffi@users.noreply.github.com>
2022-05-21 22:20:38 +0400

    доделал практическую часть и правки

--- test.py
+++ test.py
@@ -1,12 +1,6 @@
-# Import os for file nav
-import os 
-# Import callback class from sb3
-from stable_baselines3.common.callbacks import BaseCallback
 # import ppo for training
 from stable_baselines3 import PPO
 from environment import VizDoomGym
-# Import eval policy to test agent
-from stable_baselines3.common.evaluation import evaluate_policy
 import time
 
 if __name__ == '__main__':
@@ -17,15 +11,26 @@ if __name__ == '__main__':
     # Reload model from disc
     model = PPO.load('train/train_corridor/best_model_560000')
     # Evaluate mean reward for 10 games
-    mean_reward, _ = evaluate_policy(model, env, n_eval_episodes=10)
-    for episode in range(20): 
+    final_reward = 0
+    min_rew = 1e10
+    max_rew = -1e10
+    for episode in range(100): 
         obs = env.reset()
         done = False
         total_reward = 0
-        while not done: 
+        st = 0
+        while not done:
+            st += 1
             action, _ = model.predict(obs)
             obs, reward, done, info = env.step(action)
             time.sleep(0.02)
             total_reward += reward
+            print(f'Reward on step {st} in episode {episode} : {reward}')
+        final_reward += total_reward
+        min_rew = min(min_rew, total_reward)
+        max_rew = max(max_rew, total_reward)
         print('Total Reward for episode {} is {}'.format(total_reward, episode))
         time.sleep(2)
+    print(f'Maximal Reward : {max_rew}')
+    print(f'Minimal Reward : {min_rew}')
+    print(f'ep_rew_mean : {final_reward / 100}')
